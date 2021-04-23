27395acde61800b5873b39e4065c97826d9ca849
Sayali <sayali.badhe@neosoftmail.com>
2021-04-23 11:24:47 +0530

    First Commit

---
+++ GitHubExample/src/com/githubtest/GitHubTest.java
@@ -0,0 +1,12 @@
+package com.githubtest;
+
+public class GitHubTest {
+	public static void main(String[] args) {
+		String code= "SE";
+		if(code=="SE")
+			System.out.println("Software Engineer");
+		else
+			System.out.println("Not a Software Engineer");
+	}
+
+}
